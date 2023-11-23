6ff810cbc4ed192d81e48407050a13bf34643ae1
2012-Pai-Raymond <rpai@nevada.unr.edu>
2023-11-22 21:00:42 -0800

    Test.Gui Incomplete

---
+++ testgui.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter.font import Font
+
+\
+#function to be called when the button is clicked
+#def on_button_click():
+    #messagebox.showinfo("Hello World")
+#create the main window
+root = tk.Tk()
+root.geometry("1600x1200")
+#Set the window Title
+root.title("CFSA Prototype")
+
+CyberFont = Font(
+    family = "D:\Documents\CS425_Demo\Fonts",
+    size = 42,
+    weight = "bold",
+    slant = "roman",
+)
+#setting background color
+
+
+root.configure(bg="lightblue")
+#CFSA Title Widget
+label = tk.Label(root, text="CFSA Demo", font = CyberFont, bg="lightblue", pady=10)
+
+label.place(x = 800, y = 300)
+
+#button = tk.Button(root, text="Click Me, ", command = on_button_click)
+#button.pack()
+
+root.mainloop()
