63cd9baa62ef46573fac572eb8463c3dc06ef1dc
2012-Pai-Raymond <rpai@nevada.unr.edu>
2023-11-28 15:17:33 -0800

    Updated GUI with better layout

--- testgui.py
+++ testgui.py
@@ -6,6 +6,8 @@ import random
 import model
 import numpy as np
 import grammar
+import pyglet, os
+
 #function to be called when the button is clicked
 #def on_button_click():
     #messagebox.showinfo("Hello World")
@@ -35,15 +37,15 @@ import grammar
 # #button.pack()
 
   
-def compare_with_database():
-    frame = tk.Tk() 
-    frame.title("Compare With Database") 
-    frame.geometry('1600x1200') 
+def compare_with_database(root_window):
+    frame1 = tk.Toplevel(root_window) 
+    frame1.title("Compare With Database") 
+    frame1.geometry('1600x1200') 
     # Function for getting Input 
     # from textbox and printing it  
     # at label widget 
     prompts = prompt.get_prompts()
-    pmt = tk.Label(frame, text=np.random.choice(prompts, replace=False))
+    pmt = tk.Label(frame1, text=np.random.choice(prompts, replace=False))
     pmt.pack()
     def printInput():
         text = inputtxt.get("1.0", "end-1c")
@@ -64,36 +66,37 @@ def compare_with_database():
         pmt.config(text=np.random.choice(prompts, replace=False))
         inputtxt.delete("1.0", "end-1c") 
     # TextBox Creation 
-    inputtxt = tk.Text(frame, 
+    inputtxt = tk.Text(frame1, 
                     height = 50, 
                     width = 150) 
     
     inputtxt.pack() 
     
     # Button Creation 
-    printButton = tk.Button(frame, 
+    printButton = tk.Button(frame1, 
                             text = "Predict",  
                             command = printInput) 
     printButton.pack()
     
-    skipPromptButton = tk.Button(frame, 
+    skipPromptButton = tk.Button(frame1, 
                             text = "Skip Prompt",  
                             command = skipPrompt) 
     skipPromptButton.pack()  
     
     # Label Creation 
-    lbl = tk.Label(frame, text = "") 
+    lbl = tk.Label(frame1, text = "") 
     lbl.pack() 
-    frame.mainloop()
+    frame1.mainloop()
 
 
-def compare_two_prints():
+def compare_two_prints(root_window):
     result = False
     def submit():
         # model.clear_temp()
         value1 = entry1.get("1.0", "end-1c")  # Retrieve text from the first text box
         value2 = entry2.get("1.0", "end-1c")  # Retrieve text from the second text box
-        
+        print(value1)
+        print(value2)
         length1 = len(value1)
         length2 = len(value2)
         
@@ -123,71 +126,101 @@ def compare_two_prints():
         label = tk.Label(frame, text=results)
         label.pack()
 
-    # Create the main window
-    root = tk.Tk()
-    root.title("Compare Two Prints")
+    # Create the sub window
+
+    cmp = tk.Toplevel(root_window)
+    cmp.title("Compare Two Prints")
 
     prompts = prompt.get_prompts()
     text_1 = np.random.choice(prompts, replace = False)
     text_2 = np.random.choice(prompts, replace = False)
     # Create the first text box
-    label1 = tk.Label(root, text=text_1)
-    label1.grid(row=0, column=0, padx=100, pady=10, sticky="w")
+    label1 = tk.Label(cmp, text=text_1)
+    label1.grid(row=0, column=0, padx=1, pady=10, sticky="w")
 
-    entry1 = tk.Text(root, height=20, width=100)
+    entry1 = tk.Text(cmp, height=20, width=100)
     entry1.grid(row=0, column=1, padx=10, pady=10)
 
     # Create the second text box
-    label2 = tk.Label(root, text=text_2)
+    label2 = tk.Label(cmp, text=text_2)
     label2.grid(row=1, column=0, padx=100, pady=10, sticky="w")
 
-    entry2 = tk.Text(root, height=20, width=100)
+    entry2 = tk.Text(cmp, height=20, width=100)
     entry2.grid(row=1, column=1, padx=10, pady=10)
 
     # Create the submission button
-    submit_button = tk.Button(root, text="Predict", command=submit)
+    submit_button = tk.Button(cmp, text="Predict", command=submit)
     submit_button.grid(row=2, column=0, columnspan=2, pady=10)
 
-    root.mainloop()  # Start the Tkinter event loop
+    cmp.mainloop()  # Start the Tkinter event loop
 # Start the Tkinter event loop
-# root.mainloop()     
+# root.mainloop()
+# 
+
+def on_enter(button):
+    button.config(bg = '#9d9dff')
+
+def on_leave(button):
+    button.config(bg = 'SystemButtonFace')
+
 def main(): 
     # root.mainloop()
+
     frame = tk.Tk()
     frame.state('zoomed') 
     frame.title("CFSA Prototype")
     # frame.geometry('1600x1200')
+    
     CyberFont = Font(
         family = "Verdana",
         size = 42,
         weight = "bold",
         slant = "roman",
     )    
+
+    CyberFontButton = Font(
+        family = "Verdana",
+        weight = "bold",
+        slant = "roman",
+    )
     frame.configure(bg="lightblue")
+
     #CFSA Title Widget
     label = tk.Label(frame, text="CFSA Demo", font = CyberFont, bg="lightblue", pady=10)
-    label.grid(row=1, column=0, padx=100, pady=10, sticky="w") 
+    #label = tk.Label(frame, text="CFSA Demo", font = ('Starzoom-Shavian', 25), bg="lightblue", pady=10)
+    label.place(relx = .5, rely = .2, anchor = "center") 
     # Button Creation
      
     db_compare_button = tk.Button(frame, 
                             text = "Compare With Database",  
-                            command = compare_with_database)
-    db_compare_button.grid(row=1, column=1, columnspan=100, pady=10) 
+                            command = lambda: compare_with_database(frame), font = CyberFontButton, width = 35, height = 5)
+    db_compare_button.place(relx = .61, rely= .4, anchor = "center") 
     # db_compare_button.pack()
     
+    #button light up
+    db_compare_button.bind('<Enter>', lambda event, btn = db_compare_button: on_enter(btn))
+    db_compare_button.bind('<Leave>', lambda event, btn = db_compare_button: on_leave(btn))
+
     compare_two_prints_button = tk.Button(frame, 
                             text = "Compare Two Prints",  
-                            command = compare_two_prints)
-    compare_two_prints_button.grid(row=2, column=1, columnspan=100, pady=10)  
+                            command = lambda: compare_two_prints(frame), font = CyberFontButton, width = 35, height = 5)
+    compare_two_prints_button.place(relx = .37, rely = .4, anchor = "center")  
     # compare_two_prints_button.pack()
     
+    compare_two_prints_button.bind('<Enter>', lambda event, btn = compare_two_prints_button: on_enter(btn))
+    compare_two_prints_button.bind('<Leave>', lambda event, btn = compare_two_prints_button: on_leave(btn))
+
     def quit():
         frame.destroy()
     
     quit_button = tk.Button(frame, 
                             text = "Quit Application",  
-                            command = quit)
-    quit_button.grid(row=3, column=1, columnspan=100, pady=10) 
+                            command = quit, width = 30, font = CyberFontButton, height = 5, anchor = "center")
+    quit_button.place(relx = .5, rely = .8, anchor = "center") 
+
+    quit_button.bind('<Enter>', lambda event, btn = quit_button: on_enter(btn))
+    quit_button.bind('<Leave>', lambda event, btn = quit_button: on_leave(btn))
+
     # quit_button.pack()    
 
     frame.mainloop()
